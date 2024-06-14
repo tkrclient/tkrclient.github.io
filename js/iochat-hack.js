bb7fbd063afeba19eccaf6bad0074095c5b36e26
script14 <n55680642@gmail.com>
2024-06-14 12:41:12 -0700

    Update iochat-hack.js

--- js/iochat-hack.js
+++ js/iochat-hack.js
@@ -275,8 +275,8 @@
 
     /* BACKGROUNDS JS */
     // Remove the background first variable
-    /* const elementToRemove = document.querySelectorAll('#video-background');
-    
+    const elementToRemove = document.querySelectorAll('#video-background');
+
     // Pick which background variables
     const videoUrls = {
       b1: 'https://cdn.pixabay.com/video/2024/05/29/214405_large.mp4',
@@ -286,18 +286,18 @@
       b5: 'https://cdn.pixabay.com/video/2021/10/10/91562-629172467_large.mp4',
       b6: 'https://cdn.pixabay.com/video/2019/10/09/27669-365224683_large.mp4'
     };
-    
+
     // Remove background
     const rem = document.querySelector('.rem');
-    
+
     // Function to remove existing video-background elements
     function removeVideoBackground() {
       const existingVideos = document.querySelectorAll('#video-background');
-      elementToRemove.forEach(video => {
+      existingVideos.forEach(video => {
         video.remove();
       });
     }
-    
+
     // Function to create a new video-background element
     function createVideoBackground(videoUrl) {
       var video = document.createElement('video');
@@ -312,39 +312,39 @@
       video.appendChild(source);
       document.body.insertBefore(video, document.body.firstChild);
     }
-    
+
     // Event listener function for background changes
     function changeBackground(event) {
       const videoKey = event.target.classList[0];
       const videoUrl = videoUrls[videoKey];
-    
+
       if (videoUrl) {
         removeVideoBackground();
         createVideoBackground(videoUrl);
         localStorage.setItem('lastBackground', videoKey); // Store the last clicked background
       }
     }
-    
+
     // Add event listeners to background elements
     const backgroundElements = document.querySelectorAll('.b1, .b2, .b3, .b4, .b5, .b6');
     backgroundElements.forEach(element => {
       element.addEventListener('click', changeBackground);
     });
-    
+
     // Remove background click
     function remchange() {
       removeVideoBackground();
       localStorage.removeItem('lastBackground'); // Remove the stored last background
     }
-    
+
     rem.addEventListener('click', remchange);
-    
+
     // Load the last clicked background on page load
     window.addEventListener('load', () => {
       const lastBackground = localStorage.getItem('lastBackground');
       if (lastBackground && videoUrls[lastBackground]) {
         createVideoBackground(videoUrls[lastBackground]);
       }
-    }); */
+    });
 
 })();
