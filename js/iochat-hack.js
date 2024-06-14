2aaf2db62a7d50c37fa7f14a49ab65ab58e0016d
script14 <n55680642@gmail.com>
2024-06-14 13:47:44 -0700

    Update iochat-hack.js

--- js/iochat-hack.js
+++ js/iochat-hack.js
@@ -347,4 +347,24 @@
       }
     });
 
+    /* GIF AUTOMATIC LOADING SUPPORT */
+    setTimeout(function() {
+      const messages = document.querySelectorAll('.message');
+
+      function convertGifUrlToImage(message) {
+        const text = message.textContent.trim();
+        const regex = /https?:\/\/\S+\.(gif)/i;
+        const match = text.match(regex);
+
+        if (match) {
+          const img = document.createElement('img');
+          img.src = match[0];
+          message.innerHTML = '';
+          message.appendChild(img);
+        }
+      }
+
+      messages.forEach(convertGifUrlToImage);
+    }, 3000);
+
 })();
