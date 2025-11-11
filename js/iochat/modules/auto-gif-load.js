f3a7960f34497ae1dbf4849ef020ce84ea91f0ea
script26 <97182340+script26@users.noreply.github.com>
2025-11-11 11:41:05 -0700

    Create auto-gif-load.js

---
+++ js/iochat/modules/auto-gif-load.js
@@ -0,0 +1,30 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  
+    /*---------------------------------*/
+    /*- GIF automatic loading support -*/
+    /*---------------------------------*/
+    
+    setTimeout(function() {
+      const messages = document.querySelectorAll('.message');
+
+      function convertGifUrlToImage(message) {
+        const text = message.textContent.trim();
+        const regex = /https?:\/\/\S+\.(gif)/i;
+        const match = text.match(regex);
+
+        if (match) {
+          const img = document.createElement('img');
+          img.src = match[0];
+          img.style.maxWidth = '200px'; // Set the max-width to 100%
+          message.innerHTML = '';
+          message.appendChild(img);
+        }
+      }
+
+      messages.forEach(convertGifUrlToImage);
+    }, 3000);
+
+});
