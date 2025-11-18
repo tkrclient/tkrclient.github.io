aedc2f0b70c89b4c6a88934e43d0e1065e2f630a
script26 <97182340+script26@users.noreply.github.com>
2025-11-18 16:37:04 -0700

    Update buttons.js

--- js/iochat/modules/buttons.js
+++ js/iochat/modules/buttons.js
@@ -1,5 +1,65 @@
 'use strict';
 
 buttons() {
+  
+/*
+  const toggleButtonChat = document.getElementById('toggleButtonChat');
+  const chat = document.getElementById('chat');
+  console.log("sidebar");
+  const toggleButtonSidebar = document.getElementById('toggleButtonSidebar');
+  const sidebars = document.getElementById('sidebars');
+  console.log("sidebar again");
+  const toggleButtonSidebarDesktop = document.getElementById('toggleButtonSidebarDesktop');
+  const toggleButtonFullscreen = document.getElementById('toggleButtonFullscreen');
+  console.log("toggleButtonFullscreen");
+  const secondChatButton = document.getElementById('secondChatButton');
+  const secondChatServer = document.getElementById('secondChatServer');
+  console.log("secondChatServer");
+  const firstDrawButton = document.getElementById('firstDrawButton');
+  const firstDrawServer = document.getElementById('firstDrawServer');
+  console.log("firstDrawServer");
+  const secondChatButtonMobile = document.getElementById('secondChatButtonMobile');
+  console.log("secondChatServerMobile");
 
+  toggleButtonChat.addEventListener('click', () => {
+    console.log("button clicked");
+    chat.classList.toggle('active');
+  });
+  console.log("side");
+  toggleButtonSidebar.addEventListener('click', () => {
+    console.log("button clicked");
+    sidebars.classList.toggle('active');
+  });
+  console.log("side");
+  toggleButtonSidebarDesktop.addEventListener('click', () => {
+    console.log("button clicked");
+    sidebars.classList.toggle('active');
+  });
+  function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  }
+  toggleButtonFullscreen.addEventListener('click', toggleFullscreen);
+  // Second chat button
+  secondChatButton.addEventListener('click', () => {
+    console.log("button clicked");
+    secondChatServer.classList.toggle('hide');
+  });
+  // First draw button
+  firstDrawButton.addEventListener('click', () => {
+    console.log("draw button clicked");
+    firstDrawServer.classList.toggle('hide');
+  });
+  // Second chat button mobile
+  secondChatButtonMobile.addEventListener('click', () => {
+    console.log("button clicked");
+    secondChatServer.classList.toggle('hide');
+  });
+*/
+  
 };
