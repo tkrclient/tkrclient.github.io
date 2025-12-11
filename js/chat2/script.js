a08f9551aebb0b5c6366917ee36e7e16242a7a79
script26 <97182340+script26@users.noreply.github.com>
2025-12-11 10:34:34 -0700

    Update script.js

--- js/chat2/script.js
+++ js/chat2/script.js
@@ -20,8 +20,8 @@ window.onload = function() {
 	
 	// Function to set background-color and text color
 	function setColor(element, color) {
-		//element.style.backgroundColor = color;
-		element.style.color = color; // Default text color (white)
+		element.style.backgroundColor = color;
+		//element.style.color = color; // Default text color (white)
 	}
 	
 	// Function to set username
